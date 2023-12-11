e036dd9f664be47d5dc3def7b37ea732eac7dc99
j0srisk <j0srisk@gmail.com>
2023-12-11 17:15:57 -0600

    cleaned up containers page

--- app/containers/page.tsx
+++ app/containers/page.tsx
@@ -3,68 +3,24 @@ import Heading from '@/components/Heading';
 
 export default function Page() {
 	return (
-		<>
+		<div className="flex h-full w-full flex-col px-4">
 			<Heading heading="Containers" subheading="4 running" />
-			<div className="flex w-full flex-col justify-between gap-4 overflow-scroll">
-				<p className="text-2xl font-semibold">Media</p>
-				<Container
-					name={'Plex'}
-					url={'http://192.168.1.93:32400'}
-					icon={
-						'https://raw.githubusercontent.com/walkxcode/dashboard-icons/1385e150f515795aa078bdbae2b8cdafb7567368/svg/plex.svg'
-					}
-				/>
-				<Container
-					name={'Overseerr'}
-					url={'https://plex.walkxcode.dev'}
-					icon={
-						'https://raw.githubusercontent.com/walkxcode/dashboard-icons/1385e150f515795aa078bdbae2b8cdafb7567368/svg/overseerr.svg'
-					}
-				/>
-				<Container
-					name={'Sonarr'}
-					url={'https://plex.walkxcode.dev'}
-					icon={
-						'https://raw.githubusercontent.com/walkxcode/dashboard-icons/1385e150f515795aa078bdbae2b8cdafb7567368/svg/sonarr.svg'
-					}
-				/>
-				<Container
-					name={'Radarr'}
-					url={'https://plex.walkxcode.dev'}
-					icon={
-						'https://raw.githubusercontent.com/walkxcode/dashboard-icons/1385e150f515795aa078bdbae2b8cdafb7567368/svg/radarr.svg'
-					}
-				/>
-				<p className="text-2xl font-semibold">Conroe</p>
-				<Container
-					name={'Plex'}
-					url={'http://192.168.1.93:32400'}
-					icon={
-						'https://raw.githubusercontent.com/walkxcode/dashboard-icons/1385e150f515795aa078bdbae2b8cdafb7567368/svg/plex.svg'
-					}
-				/>
-				<Container
-					name={'Overseerr'}
-					url={'https://plex.walkxcode.dev'}
-					icon={
-						'https://raw.githubusercontent.com/walkxcode/dashboard-icons/1385e150f515795aa078bdbae2b8cdafb7567368/svg/overseerr.svg'
-					}
-				/>
-				<Container
-					name={'Sonarr'}
-					url={'https://plex.walkxcode.dev'}
-					icon={
-						'https://raw.githubusercontent.com/walkxcode/dashboard-icons/1385e150f515795aa078bdbae2b8cdafb7567368/svg/sonarr.svg'
-					}
-				/>
-				<Container
-					name={'Radarr'}
-					url={'https://plex.walkxcode.dev'}
-					icon={
-						'https://raw.githubusercontent.com/walkxcode/dashboard-icons/1385e150f515795aa078bdbae2b8cdafb7567368/svg/radarr.svg'
-					}
-				/>
+			<div className="flex h-full w-full flex-col justify-start gap-8 overflow-auto pb-2">
+				<div className="flex flex-col gap-2">
+					<p className="text-2xl font-semibold">Media</p>
+					<Container name={'Plex'} url={'http://192.168.1.93:32400'} />
+					<Container name={'Tautulli'} url={'http://192.168.1.93:8181'} />
+					<Container name={'Overseerr'} url={'http://192.168.1.93:5055'} />
+					<Container name={'Radarr'} url={'http://192.168.1.93:7878'} />
+					<Container name={'Sonarr'} url={'http://192.168.1.93:8989'} />
+					<Container name={'Readarr'} url={'http://192.168.1.93:8888'} />
+				</div>
+				<div className="flex flex-col gap-2">
+					<p className="text-2xl font-semibold">Unraid</p>
+					<Container name={'Unraid'} url={'http://192.168.1.93'} />
+					<Container name={'Docker'} url={'http://192.168.1.93/Docker'} />
+				</div>
 			</div>
-		</>
+		</div>
 	);
 }
--- components/Container.tsx
+++ components/Container.tsx
@@ -6,16 +6,20 @@ export default function Container({
 	url,
 }: {
 	name: string;
-	icon: string;
+	icon?: string | null | undefined;
 	url: string;
 }) {
+	const iconUrl = icon
+		? icon
+		: `https://raw.githubusercontent.com/walkxcode/dashboard-icons/1385e150f515795aa078bdbae2b8cdafb7567368/svg/${name.toLowerCase()}.svg`;
+
 	return (
 		<Link
 			href={url}
 			className="relative flex h-full w-full items-center justify-start gap-4 rounded-xl bg-zinc-500/30 p-4 "
 		>
 			<div className="flex h-14 w-14  items-center justify-center rounded-xl">
-				<img src={icon} alt="icon" className="" />
+				<img src={iconUrl} alt="icon" className="" />
 			</div>
 			<p className="text-center text-2xl font-semibold text-white">{name}</p>
 			<div className="absolute right-2 top-2 h-3 w-3 rounded-full bg-green-500" />
