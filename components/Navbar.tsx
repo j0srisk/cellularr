bb16b905a4e44763eb088b9875db13cb980baecf
j0srisk <j0srisk@gmail.com>
2023-12-21 15:09:36 -0600

    fixed navbar spacing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,79 +2,81 @@ import Link from 'next/link';
 
 export default function Navbar() {
 	return (
-		<nav className="fixed bottom-0 flex w-full flex-wrap items-center justify-between bg-black px-6 pb-9 pt-[7px] text-[#999]">
-			<Link href="/sessions" className="flex flex-col items-center gap-[7px]">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					strokeWidth={2}
-					stroke="currentColor"
-					className="h-[21px]"
-				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
-					/>
-				</svg>
+		<nav className="pb-safe fixed bottom-0 w-full bg-system-primary-dark text-[#999]">
+			<div className="flex w-full flex-wrap items-center justify-between px-6 py-[7px]">
+				<Link href="/sessions" className="flex flex-col items-center gap-[7px]">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={2}
+						stroke="currentColor"
+						className="h-[21px]"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"
+						/>
+					</svg>
 
-				<p className="text-[10px] font-[510]">Sessions</p>
-			</Link>
-			<Link href="/search" className="flex flex-col items-center gap-[7px]">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					strokeWidth={2}
-					stroke="currentColor"
-					className="h-[21px]"
-				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-					/>
-				</svg>
+					<p className="text-[10px] font-[510]">Sessions</p>
+				</Link>
+				<Link href="/search" className="flex flex-col items-center gap-[7px]">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={2}
+						stroke="currentColor"
+						className="h-[21px]"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+						/>
+					</svg>
 
-				<p className="text-[10px] font-[510]">Search</p>
-			</Link>
-			<Link href="/" className="flex flex-col items-center gap-[7px]">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					strokeWidth={2}
-					stroke="currentColor"
-					className="h-[21px]"
-				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
-					/>
-				</svg>
+					<p className="text-[10px] font-[510]">Search</p>
+				</Link>
+				<Link href="/" className="flex flex-col items-center gap-[7px]">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={2}
+						stroke="currentColor"
+						className="h-[21px]"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m8.25 3v6.75m0 0-3-3m3 3 3-3M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+						/>
+					</svg>
 
-				<p className="text-[10px] font-[510]">Downloads</p>
-			</Link>
-			<Link href="/containers" className="flex flex-col items-center gap-[7px]">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					strokeWidth={2}
-					stroke="currentColor"
-					className="h-[21px]"
-				>
-					<path
-						strokeLinecap="round"
-						strokeLinejoin="round"
-						d="M5.25 14.25h13.5m-13.5 0a3 3 0 0 1-3-3m3 3a3 3 0 1 0 0 6h13.5a3 3 0 1 0 0-6m-16.5-3a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3m-19.5 0a4.5 4.5 0 0 1 .9-2.7L5.737 5.1a3.375 3.375 0 0 1 2.7-1.35h7.126c1.062 0 2.062.5 2.7 1.35l2.587 3.45a4.5 4.5 0 0 1 .9 2.7m0 0a3 3 0 0 1-3 3m0 3h.008v.008h-.008v-.008Zm0-6h.008v.008h-.008v-.008Zm-3 6h.008v.008h-.008v-.008Zm0-6h.008v.008h-.008v-.008Z"
-					/>
-				</svg>
+					<p className="text-[10px] font-[510]">Downloads</p>
+				</Link>
+				<Link href="/containers" className="flex flex-col items-center gap-[7px]">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={2}
+						stroke="currentColor"
+						className="h-[21px]"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="M5.25 14.25h13.5m-13.5 0a3 3 0 0 1-3-3m3 3a3 3 0 1 0 0 6h13.5a3 3 0 1 0 0-6m-16.5-3a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3m-19.5 0a4.5 4.5 0 0 1 .9-2.7L5.737 5.1a3.375 3.375 0 0 1 2.7-1.35h7.126c1.062 0 2.062.5 2.7 1.35l2.587 3.45a4.5 4.5 0 0 1 .9 2.7m0 0a3 3 0 0 1-3 3m0 3h.008v.008h-.008v-.008Zm0-6h.008v.008h-.008v-.008Zm-3 6h.008v.008h-.008v-.008Zm0-6h.008v.008h-.008v-.008Z"
+						/>
+					</svg>
 
-				<p className="text-[10px] font-[510]">Containers</p>
-			</Link>
+					<p className="text-[10px] font-[510]">Containers</p>
+				</Link>
+			</div>
 		</nav>
 	);
 }
