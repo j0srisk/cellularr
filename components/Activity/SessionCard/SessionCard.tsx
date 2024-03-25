b33c9b68966ca72954ca496cb7ad0d86d7f8305c
j0srisk <j0srisk@gmail.com>
2024-03-25 13:05:16 -0500

    unified session and download cards

--- components/Activity/SessionCard/SessionCard.tsx
+++ components/Activity/SessionCard/SessionCard.tsx
@@ -57,92 +57,96 @@ export default function SessionCard({ session }: { session: Session }) {
 						priority={true}
 					/>
 				)}
-				<div className="relative h-1.5 w-full bg-fill-tetiary-light dark:bg-fill-tetiary-dark">
-					<div
-						style={{ width: session.transcode_progress + '%' }}
-						className="absolute h-full bg-fill-secondary-light dark:bg-fill-secondary-dark"
-					></div>
-					<div
-						style={{ width: session.progress_percent + '%' }}
-						className="absolute h-full bg-system-orange-light dark:bg-system-orange-dark"
-					></div>
-				</div>
-				<div className="flex w-full items-start justify-start gap-3 p-3">
-					<div className="flex h-fit w-full flex-col justify-start">
-						{mediaType === MediaType.MOVIE && (
-							<>
-								<p className="text-body-emphasized">{session.title}</p>
-								<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
-									{session.year}
-								</p>
-							</>
+
+				<div className="flex flex-col gap-4 p-4">
+					<div className="flex w-full justify-start gap-3">
+						{mediaType !== MediaType.MUSIC && (
+							<PosterCard
+								title={session.title}
+								id={parseInt(tmdbId)}
+								posterPath={mediaDetails.posterPath}
+								mediaType={mediaType}
+								className="h-full w-14 rounded-[5px] border-none shadow-none"
+							/>
 						)}
-						{mediaType === MediaType.TV && (
-							<>
-								<p className="text-body-emphasized">{session.grandparent_title}</p>
-								<p className="hidden text-footnote text-label-secondary-light dark:text-label-secondary-dark">
-									{session.title}
-								</p>
+						<div className="flex h-fit w-full flex-col justify-between">
+							{mediaType === MediaType.MOVIE && (
+								<>
+									<p className="text-title-2-emphasized">{session.title}</p>
+									<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
+										{session.year}
+									</p>
+								</>
+							)}
+							{mediaType === MediaType.TV && (
+								<>
+									<p className="text-title-2-emphasized">{session.grandparent_title}</p>
+									<p className="hidden text-footnote text-label-secondary-light dark:text-label-secondary-dark">
+										{session.title}
+									</p>
+									<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
+										S{session.parent_media_index} • E{session.media_index}
+									</p>
+								</>
+							)}
+							<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
+								{session.state !== 'paused'
+									? formattedDuration + ' left (' + timeString + ')'
+									: 'Paused (' + formattedDuration + ' left)'}
+							</p>
+						</div>
+					</div>
+					<div className="relative h-1.5 w-full overflow-hidden rounded-sm bg-fill-tetiary-light dark:bg-fill-tetiary-dark">
+						<div
+							style={{ width: session.transcode_progress + '%' }}
+							className="absolute h-full bg-fill-secondary-light dark:bg-fill-secondary-dark"
+						></div>
+						<div
+							style={{ width: session.progress_percent + '%' }}
+							className="absolute h-full bg-system-orange-light dark:bg-system-orange-dark"
+						></div>
+					</div>
+					<div className="flex items-center gap-3">
+						<div className="flex items-center gap-1.5">
+							<Image
+								src={session.user_thumb}
+								alt={session.username}
+								height={100}
+								width={100}
+								className="h-5 w-5 rounded-full"
+								priority={true}
+							/>
+							<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
+								{session.username}
+							</p>
+						</div>
+						{location && (
+							<div className="flex hidden items-center gap-1">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									viewBox="0 0 24 24"
+									fill="currentColor"
+									className="h-4 w-4 text-label-primary-light dark:text-label-primary-dark"
+								>
+									<path
+										fillRule="evenodd"
+										d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+										clipRule="evenodd"
+									/>
+								</svg>
+
 								<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
-									S{session.parent_media_index} • E{session.media_index}
-								</p>
-							</>
-						)}
-						<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
-							{session.state !== 'paused'
-								? formattedDuration + ' left (' + timeString + ')'
-								: 'Paused (' + formattedDuration + ' left)'}
-						</p>
-						<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
-							{session.username}
-							{location && (
-								<span>
-									{' ('}
 									{location.city && location.region
 										? `${location.city}, ${location.region}`
 										: location.city || location.region}
-									{')'}
-								</span>
-							)}
-						</p>
-					</div>
-					{mediaType !== MediaType.MUSIC && (
-						<PosterCard
-							title={session.title}
-							id={parseInt(tmdbId)}
-							posterPath={mediaDetails.posterPath}
-							mediaType={mediaType}
-							className="w-14 rounded-[5px] border-none shadow-none"
-						/>
-					)}
-				</div>
-
-				<div className="hidden items-center gap-3 p-3">
-					<div className="flex h-full w-14 items-center justify-center">
-						<Image
-							src={session.user_thumb}
-							alt={session.username}
-							height={100}
-							width={100}
-							className="h-10 w-10 rounded-full"
-							priority={true}
-						/>
-					</div>
-
-					<div className="flex h-fit min-h-[54px] flex-col items-start justify-center">
-						<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
-							{session.username}
-						</p>
-						<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
-							{session.player}
-						</p>
-						{location && (
-							<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
-								{location.city && location.region
-									? `${location.city}, ${location.region}`
-									: location.city || location.region}
-							</p>
+								</p>
+							</div>
 						)}
+						<div className="flex flex-1 items-center justify-end gap-2">
+							<p className="text-footnote-emphasized tracking-tight text-system-orange-light dark:text-system-orange-dark">
+								See Details
+							</p>
+						</div>
 					</div>
 				</div>
 			</Card>
--- components/Downloads/DownloadCard/index.tsx
+++ components/Downloads/DownloadCard/index.tsx
@@ -4,9 +4,9 @@ import Card from '@/components/Common/Card';
 
 export default function DownloadCard({ download }: { download: Download }) {
 	return (
-		<Card key={download.name} className="flex w-full gap-4 p-4">
+		<Card key={download.name} className="flex w-full gap-3 p-4">
 			<div className="flex w-full flex-col">
-				<p className="w-full truncate text-start text-body-emphasized">{download.name}</p>
+				<p className="w-full truncate text-start text-title-2-emphasized">{download.name}</p>
 				<p className="truncate text-footnote text-label-secondary-light dark:text-label-secondary-dark">
 					{download.size / 1024 / 1024 > 1024
 						? (download.size / 1024 / 1024 / 1024).toFixed(2) + ' GB'
@@ -31,92 +31,93 @@ export default function DownloadCard({ download }: { download: Download }) {
 					></div>
 				</div>
 			</div>
-			{download.state === 'Downloading' ||
-				(download.state === 'Seeding' && (
-					<div className="flex w-full items-center justify-between gap-4">
-						<div className="flex items-center gap-4">
-							{download.state === 'Downloading' && (
-								<div className="flex items-center gap-1">
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										strokeWidth={2}
-										stroke="currentColor"
-										className="h-5 w-5 stroke-label-secondary-light dark:stroke-label-secondary-dark"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											d="M12 9.75v6.75m0 0-3-3m3 3 3-3m-8.25 6a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z"
-										/>
-									</svg>
-									<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
-										{(download.downloadSpeed / 1024 / 1024).toFixed(2)} MB/s
-									</p>
-								</div>
-							)}
-							{download.uploadSpeed !== undefined && download.state === 'Seeding' && (
-								<div className="flex items-center gap-1">
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										strokeWidth={2}
-										stroke="currentColor"
-										className="h-5 w-5 stroke-label-secondary-light dark:stroke-label-secondary-dark"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z"
-										/>
-									</svg>
 
-									<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
-										{(download.uploadSpeed / 1024 / 1024).toFixed(2)} MB/s
-									</p>
-								</div>
-							)}
-							{download.remaining !== 0 && download.state === 'Downloading' && (
-								<div className="flex items-center gap-1">
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										strokeWidth={2}
-										stroke="currentColor"
-										className="h-5 w-5 stroke-label-secondary-light dark:stroke-label-secondary-dark"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-										/>
-									</svg>
-
-									<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
-										{formatDurationSeconds(Math.round(download.eta / 1000))}
-									</p>
-								</div>
-							)}
+			<div className="flex w-full items-center justify-between gap-4">
+				<div className="flex items-center gap-4">
+					{download.state === 'Downloading' && (
+						<div className="flex items-center gap-1">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={2}
+								stroke="currentColor"
+								className="h-5 w-5 stroke-label-secondary-light dark:stroke-label-secondary-dark"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M12 9.75v6.75m0 0-3-3m3 3 3-3m-8.25 6a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z"
+								/>
+							</svg>
+							<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
+								{(download.downloadSpeed / 1024 / 1024).toFixed(2)} MB/s
+							</p>
 						</div>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.75}
-							stroke="currentColor"
-							className="hidden h-7 w-7 stroke-label-secondary-light dark:stroke-label-secondary-dark"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-							/>
-						</svg>
-					</div>
-				))}
+					)}
+					{download.uploadSpeed !== undefined && download.state === 'Seeding' && (
+						<div className="flex items-center gap-1">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={2}
+								stroke="currentColor"
+								className="h-5 w-5 stroke-label-secondary-light dark:stroke-label-secondary-dark"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z"
+								/>
+							</svg>
+
+							<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
+								{(download.uploadSpeed / 1024 / 1024).toFixed(2)} MB/s
+							</p>
+						</div>
+					)}
+					{download.remaining !== 0 && download.state === 'Downloading' && (
+						<div className="flex items-center gap-1">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								strokeWidth={2}
+								stroke="currentColor"
+								className="h-5 w-5 stroke-label-secondary-light dark:stroke-label-secondary-dark"
+							>
+								<path
+									strokeLinecap="round"
+									strokeLinejoin="round"
+									d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+								/>
+							</svg>
+
+							<p className="text-footnote text-label-secondary-light dark:text-label-secondary-dark">
+								{formatDurationSeconds(Math.round(download.eta / 1000))}
+							</p>
+						</div>
+					)}
+				</div>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					strokeWidth={1.75}
+					stroke="currentColor"
+					className="hidden h-7 w-7 stroke-label-secondary-light dark:stroke-label-secondary-dark"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+					/>
+				</svg>
+				<p className="text-footnote-emphasized tracking-tight text-system-blue-light dark:text-system-blue-dark">
+					See Details
+				</p>
+			</div>
 		</Card>
 	);
 }
